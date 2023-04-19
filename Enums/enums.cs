e2c1554a91bfdb16487c1010fcc699f735af3306
Van Quoc Nguyen <33346198+VANQUOC81@users.noreply.github.com>
2023-04-19 10:27:11 +0200

    Second commit Seems not all files where staged. Commit again.

--- Enums/enums.cs
+++
@@ -1,8 +0,0 @@
-namespace Enums
-{
-    public enum CodeChallengeSource
-    {
-        LeetCode,
-        Hankerrank
-    }
-}
---
+++ Tests/TimeConversionTests.cs
@@ -0,0 +1,22 @@
+using Source.Hackerrank;
+
+namespace CodeChallenges.Tests
+{
+    [TestClass]
+    public class TimeConversionTests
+    {
+        [TestMethod]
+        public void ConvertTime_StringMeridiemTime_Returns24HoursFormat()
+        {
+            // Arrange
+            var codeChallenge = new TimeConversion();
+            const string expected = "19:05:45";
+
+            // Act
+            var actual = codeChallenge.Execute();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
