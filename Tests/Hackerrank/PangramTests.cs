1e474f79545ce6382dfda658ff60c53fceb3f5c3
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-10-06 11:49:53 +0200

    add pangram test

---
+++ Tests/Hackerrank/PangramTests.cs
@@ -0,0 +1,22 @@
+using Common.Hackerrank;
+
+namespace CodeChallenges.Tests.Hackerrank
+{
+    [TestClass]
+    public class PangramTests
+    {
+        [TestMethod]
+        public void GetPangram_String_ReturnsPangram()
+        {
+            // Arrange
+            var codeChallenge = new Pangram();
+            const string expected = "";
+
+            // Act
+            var actual = codeChallenge.Execute();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
