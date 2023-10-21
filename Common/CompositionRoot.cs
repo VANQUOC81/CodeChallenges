8a46b1d91bef778fbd8ea39db5a5b91870332e91
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-10-21 21:33:44 +0200

    register SparseArrays

--- Common/CompositionRoot.cs
+++ Common/CompositionRoot.cs
@@ -21,6 +21,7 @@ public static class CompositionRoot
         services.AddSingleton<ICodeChallenge, RemoveDuplicatesfromSortedArray>();
         services.AddSingleton<ICodeChallenge, RomanToInteger>();
         services.AddSingleton<ICodeChallenge, RotateArray>();
+        services.AddSingleton<ICodeChallenge, SparseArrays>();
         
         return services;
     }
