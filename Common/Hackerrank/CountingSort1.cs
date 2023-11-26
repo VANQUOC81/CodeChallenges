faf79da7d0b29d038a2f02791feac64518e00f91
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-11-26 16:48:15 +0100

    Add countingsort unit test

--- Common/Hackerrank/CountingSort1.cs
+++ Common/Hackerrank/CountingSort1.cs
@@ -15,19 +15,19 @@ public class CountingSort1 : ICodeChallenge
         List<int> inputList = new() { 63, 25, 73, 1, 98, 73, 56, 84 /* add your elements here */ };
 
         // Calling counting sort function
-        List<int> result = countingSort(inputList);
+        List<int> result = CountingSort(inputList);
 
         return string.Join(",", result);
     }
 
     /*
-     * Complete the 'countingSort' function below.
+     * Complete the 'CountingSort' function below.
      *
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-    public static List<int> countingSort(List<int> arr)
+    public static List<int> CountingSort(List<int> arr)
     {
         // Create a frequency array with 100 elements initialized to 0
         int[] frequency = new int[100];
