e8bb3747cd42f766df529f16bb53b79e93645cde
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-12-01 11:53:19 +0100

    Convert DiagonalDifference

--- Common/Hackerrank/DiagonalDifference.cs
+++ Common/Hackerrank/DiagonalDifference.cs
@@ -1,3 +1,59 @@
+using Common.Enums;
+using Common.Interfaces;
+
+namespace Common.Hackerrank;
+
+public class DiagonalDifference : ICodeChallenge
+{
+    public CodeChallengeSource CodeChallengeSource => CodeChallengeSource.Hankerrank;
+
+    public int CodeChallengeNumber => throw new NotImplementedException("Hankerrank doesn't have numbers");
+
+    public string Execute()
+    {
+        // Sample input
+        List<List<int>> inputList = new()
+        {
+            new List<int>() { 11, 2, 4 },
+            new List<int>() { 4, 5, 6 },
+            new List<int>() { 10, 8, -12 }
+        };
+
+        // Calling function
+        var result = GetDiagonalDifference(inputList);
+
+        return result.ToString();
+    }
+
+/*
+     * Complete the 'GetDiagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int GetDiagonalDifference(List<List<int>> arr)
+    {
+int primarySum = 0;
+        int secondarySum = 0;
+
+        int n = arr.Count;
+
+        for (int i = 0; i < n; i++)
+        {
+            primarySum += arr[i][i];
+            secondarySum += arr[i][n - i - 1];
+        }
+
+        return Math.Abs(primarySum - secondarySum);
+    }
+
+    public void GetInputValuesCommands()
+    {
+        throw new NotImplementedException();
+    }
+}
+
 // using System.CodeDom.Compiler;
 // using System.Collections.Generic;
 // using System.Collections;
